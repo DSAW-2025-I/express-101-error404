6fc617086e81a34e11962a9813e1d6c78365e974
LaMajoP <majopalominoc@gmail.com>
2025-03-19 07:15:11 -0500

    se agrego el endopint con un ID y se ignoro la carpeta de node_modules

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 
-const user1 = [{nombres: "Maria Jose", apellidos: "Palomino Carreño", email: "mariapalca@unisabana.edu.co", id: "0000321271"}];
-const user2 = [{nombres: "Samuel", apellidos: "Guerrero Arcos", email: "nose@unisabana.edu.co", id: "0000nose"}];
+const user1 = [{name: "Maria Jose", lastName: "Palomino Carreño", email: "mariapalca@unisabana.edu.co", id: "0000321271"}];
+const user2 = [{name: "Samuel", lastName: "Guerrero Arcos", email: "samuelguar@unisabana.edu.co", id: "0000323500"}];
 
-app.get('/user-info/1', (req, res) => {
-    res.json(user1);
-});
-
-app.get('/user-info/2', (req, res) => {
-    res.json(user2);
+app.get('/user-info/:id', (req, res) => { //funcion callback 
+    const id = parseInt(req.params.id); //para convertir el parametro que es string a un integer 
+    
+    if (id === 1) { //usamos una comparacion estricta 
+        res.json(user1); //.json para retorar un archivo .json
+    } else if (id === 2) {
+        res.json(user2);
+    } else {
+        res.status(404).json({error:"404 Usuario no encontrado"});
+    }
 });
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
-  });
+});
